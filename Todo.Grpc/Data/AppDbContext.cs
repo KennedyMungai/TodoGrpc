0238c61dbf6f6db3b57089b596ae7955e54acb99
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-08-03 09:42:48 +0300

    Created the DbContext for the application

---
+++ Todo.Grpc/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.Grpc.Models;
+
+namespace Todo.Grpc.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<TodoModel> Todos { get; set; }
+}
