300c5f74ea9d4e3447088c4e04f51af70d4f57f2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-08-03 09:44:23 +0300

    Added the DbContext service to the application

--- Todo.Grpc/Program.cs
+++ Todo.Grpc/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.Grpc.Data;
 using Todo.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite("Data Source=Todos.db");
+});
 
 var app = builder.Build();
 
