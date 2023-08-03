47f1973b1422b084578d6b822bf25e11ca886d52
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-08-03 11:21:54 +0300

    Wrote the base code to the ToDoService

---
+++ Todo.Grpc/Services/ToDoItService.cs
@@ -0,0 +1,39 @@
+using Grpc.Core;
+using Todo.Grpc.Data;
+
+namespace Todo.Grpc.Services;
+
+public class ToDoItService : ToDoIt.ToDoItBase
+{
+    private readonly AppDbContext _context;
+
+    public ToDoItService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public override Task<CreateToDoResponse> CreateTodo(CreateToDoRequest request, ServerCallContext context)
+    {
+        return base.CreateTodo(request, context);
+    }
+
+    public override Task<ReadToDoResponse> ReadToDo(ReadToDoRequest request, ServerCallContext context)
+    {
+        return base.ReadToDo(request, context);
+    }
+
+    public override Task<GetAllResponse> ListToDo(GetAllRequest request, ServerCallContext context)
+    {
+        return base.ListToDo(request, context);
+    }
+
+    public override Task<UpdateToDoResponse> UpdateToDo(UpdateToDoRequest request, ServerCallContext context)
+    {
+        return base.UpdateToDo(request, context);
+    }
+
+    public override Task<DeleteToDoResponse> DeleteTodo(DeleteToDoRequest request, ServerCallContext context)
+    {
+        return base.DeleteTodo(request, context);
+    }
+}
